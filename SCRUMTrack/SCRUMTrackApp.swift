697fca46af307e28ec894f67c89b8244e53b4659
snipes1111 <km-92@mail.ru>
2024-02-09 17:31:30 +0100

    Initial Commit

---
+++ SCRUMTrack/SCRUMTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCRUMTrackApp.swift
+//  SCRUMTrack
+//
+//  Created by user on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SCRUMTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
